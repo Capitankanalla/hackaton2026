ec71de25bba450f648de47f41dd84de96a36bcbb
AlexBonetP <alex.derigs@gmail.com>
2026-05-15 21:09:47 +0200

    inhabilito la feina feta amb el gpt, tota improductiva i trencadora

--- backend/controllers/marketMap.js
+++ backend/controllers/marketMap.js
@@ -1,10 +1,10 @@
-module.exports = {
-  uiToRegion: {
-    españa: "EU",
-    usa: "US",
-    europe: "EU",
-    asia: "ASIA",
-    crypto: "CRYPTO",
-    forex: "FOREX"
-  }
-};
+// module.exports = {
+//   uiToRegion: {
+//     españa: "EU",
+//     usa: "US",
+//     europe: "EU",
+//     asia: "ASIA",
+//     crypto: "CRYPTO",
+//     forex: "FOREX"
+//   }
+// };
