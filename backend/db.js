be4ab2ab6cb733d72510feee1bf3bc1576bc1bd4
Capitankanalla <nanoxtechnologies@gmail.com>
2026-05-18 14:30:24 +0200

    refactor db.js per treballar tant en local com en web

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,32 @@
 // backend/db.js
+
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "2209",
-  database: process.env.DB_NAME || "marketPulse",
+  host: process.env.MYSQLHOST || "localhost",
+  user: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || "2209",
+  database: process.env.MYSQLDATABASE || "marketPulse",
+  port: process.env.MYSQLPORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 module.exports = { db };
+
+// const mysql = require("mysql2/promise");
+// require("dotenv").config();
+
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST || "localhost",
+//   user: process.env.DB_USER || "root",
+//   password: process.env.DB_PASS || "2209",
+//   database: process.env.DB_NAME || "marketPulse",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// module.exports = { db };
